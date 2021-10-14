b8d81db4888160992bf5ddd452567fac6845d1cd
thuy17 <thuytienguyen17@gmail.com>
2021-10-13 19:56:51 -0500

    add def

--- app/controllers/names_controller.rb
+++ app/controllers/names_controller.rb
@@ -1,2 +1,10 @@
 class NamesController < ApplicationController
+  def first_letter
+    name = params["name"].upcase
+    if name = "ani"
+    render json: {message: "HOOOO"}   
+    else
+    render json: {message: "rrrrrrr"}
+    end
+  end
 end
