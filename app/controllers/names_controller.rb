690b0c3e6290e7270f5714e5c8c296d8955c32f6
thuy17 <thuytienguyen17@gmail.com>
2021-10-13 20:10:47 -0500

    add if condition

--- app/controllers/names_controller.rb
+++ app/controllers/names_controller.rb
@@ -1,10 +1,11 @@
 class NamesController < ApplicationController
   def first_letter
     name = params["name"].upcase
-    if name = "ani"
-    render json: {message: "HOOOO"}   
+    if name[0] == "A"
+      the_message = "Your name begins with the first letter of the alphabet"  
     else
-    render json: {message: "rrrrrrr"}
+        the_message = "Your name is cool"
     end
+    render json: {message: the_message}
   end
 end
